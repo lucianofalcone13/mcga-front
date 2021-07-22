3c7c6e6866b9dc35ed6c6aa5069cfeb3e59732b6
Luciano Falcone <lucianoafalcone@gmail.com>
2021-07-22 15:38:08 -0300

    setup robot bold

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { store } from "./redux/store";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Login from "./components/routes/login";
 import './assets/fonts/Roboto-Regular.ttf'
+import './assets/fonts/Roboto-Bold.ttf'
 
 function App() {
   return (
