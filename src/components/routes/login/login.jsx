5ee1387e1f977ce12e1fcdeff999371d0207bb11
Luciano Falcone <lucianoafalcone@gmail.com>
2021-07-22 16:26:52 -0300

    Added email and inputs validations

--- src/components/routes/login/login.jsx
+++ src/components/routes/login/login.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router";
+import { mapError } from "../../../helpers/error-format";
+import { isValidEmail } from "../../../helpers/validations";
 import Button from "../../shared-components/button/button";
 import { TextField } from "../../shared-components/input/textField";
+import { ErrorLabel } from "../../shared-components/label/errorLabel";
 import css from "./login.module.css";
+
 const Login = () => {
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
+  const [emailFocused, setEmailFocused] = useState(false);
+  const [passwordFocused, setPasswordFocused] = useState(false);
+  const [error, setError] = useState("");
   const history = useHistory();
   const handleChange = (event) => {
     event.persist();
@@ -17,12 +24,39 @@ const Login = () => {
     }
   };
 
+  useEffect(() => {
+    if (userEmail && !isValidEmail(userEmail)) {
+      setError("InvalidEmail");
+    } else {
+      setError("");
+    }
+    if (!error && userEmail && userPassword) {
+      setError("");
+    }
+  }, [userEmail, userPassword, error]);
+
+  const handleBlur = (event) => {
+    event.persist();
+    if (event.target.name === "password") {
+      setPasswordFocused(true);
+    } else {
+      setEmailFocused(true);
+    }
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     history.push("/tasks");
   };
 
-  const isButtonDisabled = () => !!!userEmail || !!!userPassword;
+  useEffect(() => {
+    if (wasInputFocused() && (!!!userEmail || !!!userPassword)) {
+      setError("Incompleted");
+    }
+  }, [error, userEmail, userPassword, emailFocused, passwordFocused]);
+
+  const isButtonDisabled = () => !!!userEmail || !!!userPassword || error;
+  const wasInputFocused = () => emailFocused && passwordFocused;
 
   return (
     <div className={css.container}>
@@ -30,18 +64,20 @@ const Login = () => {
         <h1>SIGN IN</h1>
         <div className={css.inputsWrapper}>
           <TextField
-            autoFocus
             label="Email"
             onChange={handleChange}
-            name="password"
+            name="email"
+            onBlur={handleBlur}
           />
           <br />
           <TextField
             label="Password"
             type="password"
             onChange={handleChange}
-            name="email"
+            name="password"
+            onBlur={handleBlur}
           />
+          <ErrorLabel message={mapError[error]} />
         </div>
         <Button type="submit" size="height" disabled={isButtonDisabled()}>
           Login
